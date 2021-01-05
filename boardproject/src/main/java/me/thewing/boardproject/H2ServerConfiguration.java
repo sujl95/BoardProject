b1b2ec11072db4b1003dbcffd6a2ab45a660efcf
sujl95 <sujl95@naver.com>
2021-01-06 00:03:33 +0900

    Embeded Console

---
+++ boardproject/src/main/java/me/thewing/boardproject/H2ServerConfiguration.java
@@ -0,0 +1,25 @@
+package me.thewing.boardproject;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.h2.tools.Server;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2ServerConfiguration {
+	// @Bean
+	// public Server h2TcpServer() throws SQLException {
+	// 	return Server.createTcpServer().start();
+	// }
+
+	@Bean
+	@ConfigurationProperties("spring.datasource.hikari") // yml의 설정값을 Set한다.
+	public DataSource dataSource() throws SQLException {
+		Server.createTcpServer("-tcp", "-tcpAllowOthers", "-ifNotExists", "-tcpPort", "9092").start();
+		return new com.zaxxer.hikari.HikariDataSource();
+	}
+}
