9c23557883afa55864f5d018b0561b6a2817ebf6
sujl95 <sujl95@naver.com>
2021-01-16 00:31:26 +0900

    TDD refactoring

--- boardproject/src/main/java/me/thewing/boardproject/controller/TestController.java
+++ boardproject/src/main/java/me/thewing/boardproject/controller/TestController.java
@@ -1,96 +1,96 @@
-package me.thewing.boardproject.controller;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.HashMap;
-
-import javax.net.ssl.HttpsURLConnection;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.RequiredArgsConstructor;
-import me.thewing.boardproject.domain.TestDto;
-import me.thewing.boardproject.service.TestService;
-
-@RestController
-@RequiredArgsConstructor
-@RequestMapping(value = "api/test")
-public class TestController {
-	private final TestService testService;
-
-	private Double x ;
-	// private Double x = 126.716316;
-	private Double y ;
-	// private Double y = 37.599191;
-
-	private String ApiUrl;
-
-	private static String APIKey = "KakaoAK 109569371036c4d6f2f7a904ae262661";
-
-	HashMap<String, Object> map = new HashMap<>();
-
-	@PostMapping("test")
-	public ResponseEntity<?> checkApi(TestDto testDto) {
-		System.out.println(testDto.toString());
-		x = testDto.getX();
-		y = testDto.getY();
-		String addr = "";
-		ApiUrl = "https://dapi.kakao.com/v2/local/geo/coord2address.json?input_coord=WGS84&y="+y+"&x="+x;
-		try{
-			addr = getRegionAddress(getJSONData(ApiUrl));
-		}catch(Exception e){
-			System.out.println("error");
-			e.printStackTrace();
-		}
-		return new ResponseEntity<>(addr,HttpStatus.OK);
-	}
-
-	private static String getJSONData(String apiUrl) throws Exception {
-		StringBuilder jsonString = new StringBuilder(new String());
-		String buf;
-		URL url = new URL(apiUrl);
-		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("X-Requested-With", "curl");
-		conn.setRequestProperty("Authorization", APIKey);
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(
-				conn.getInputStream(), "UTF-8"));
-		while ((buf = br.readLine()) != null) {
-			jsonString.append(buf);
-		}
-		return jsonString.toString();
-	}
-	private static String getRegionAddress(String jsonString) throws Exception {
-		String value = "";
-		JSONObject jObj = (JSONObject) JSONValue.parse(jsonString);
-		JSONObject meta = (JSONObject) jObj.get("meta");
-		long size = (long) meta.get("total_count");
-		if(size>0){
-			JSONArray jArray = (JSONArray) jObj.get("documents");
-			JSONObject subJobj = (JSONObject) jArray.get(0);
-			JSONObject roadAddress =  (JSONObject) subJobj.get("road_address");
-			if(roadAddress == null){
-				JSONObject subsubJobj = (JSONObject) subJobj.get("address");
-				value = (String) subsubJobj.get("address_name");
-			}else{
-				value = (String) roadAddress.get("address_name");
-			}
-			if(value.equals("") || value==null){
-				subJobj = (JSONObject) jArray.get(1);
-				subJobj = (JSONObject) subJobj.get("address");
-				value =(String) subJobj.get("address_name");
-			}
-		}
-		return value;
-	}
-
-}
+// package me.thewing.boardproject.controller;
+//
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// import java.net.URL;
+// import java.util.HashMap;
+//
+// import javax.net.ssl.HttpsURLConnection;
+//
+// import org.json.simple.JSONArray;
+// import org.json.simple.JSONObject;
+// import org.json.simple.JSONValue;
+// import org.springframework.http.HttpStatus;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+//
+// import lombok.RequiredArgsConstructor;
+// import me.thewing.boardproject.domain.TestDto;
+// import me.thewing.boardproject.service.TestService;
+//
+// @RestController
+// @RequiredArgsConstructor
+// @RequestMapping(value = "api/test")
+// public class TestController {
+// 	private final TestService testService;
+//
+// 	private Double x ;
+// 	// private Double x = 126.716316;
+// 	private Double y ;
+// 	// private Double y = 37.599191;
+//
+// 	private String ApiUrl;
+//
+// 	private static String APIKey = "KakaoAK 109569371036c4d6f2f7a904ae262661";
+//
+// 	HashMap<String, Object> map = new HashMap<>();
+//
+// 	@PostMapping("test")
+// 	public ResponseEntity<?> checkApi(TestDto testDto) {
+// 		System.out.println(testDto.toString());
+// 		x = testDto.getX();
+// 		y = testDto.getY();
+// 		String addr = "";
+// 		ApiUrl = "https://dapi.kakao.com/v2/local/geo/coord2address.json?input_coord=WGS84&y="+y+"&x="+x;
+// 		try{
+// 			addr = getRegionAddress(getJSONData(ApiUrl));
+// 		}catch(Exception e){
+// 			System.out.println("error");
+// 			e.printStackTrace();
+// 		}
+// 		return new ResponseEntity<>(addr,HttpStatus.OK);
+// 	}
+//
+// 	private static String getJSONData(String apiUrl) throws Exception {
+// 		StringBuilder jsonString = new StringBuilder(new String());
+// 		String buf;
+// 		URL url = new URL(apiUrl);
+// 		HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+// 		conn.setRequestMethod("GET");
+// 		conn.setRequestProperty("X-Requested-With", "curl");
+// 		conn.setRequestProperty("Authorization", APIKey);
+//
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(
+// 				conn.getInputStream(), "UTF-8"));
+// 		while ((buf = br.readLine()) != null) {
+// 			jsonString.append(buf);
+// 		}
+// 		return jsonString.toString();
+// 	}
+// 	private static String getRegionAddress(String jsonString) throws Exception {
+// 		String value = "";
+// 		JSONObject jObj = (JSONObject) JSONValue.parse(jsonString);
+// 		JSONObject meta = (JSONObject) jObj.get("meta");
+// 		long size = (long) meta.get("total_count");
+// 		if(size>0){
+// 			JSONArray jArray = (JSONArray) jObj.get("documents");
+// 			JSONObject subJobj = (JSONObject) jArray.get(0);
+// 			JSONObject roadAddress =  (JSONObject) subJobj.get("road_address");
+// 			if(roadAddress == null){
+// 				JSONObject subsubJobj = (JSONObject) subJobj.get("address");
+// 				value = (String) subsubJobj.get("address_name");
+// 			}else{
+// 				value = (String) roadAddress.get("address_name");
+// 			}
+// 			if(value.equals("") || value==null){
+// 				subJobj = (JSONObject) jArray.get(1);
+// 				subJobj = (JSONObject) subJobj.get("address");
+// 				value =(String) subJobj.get("address_name");
+// 			}
+// 		}
+// 		return value;
+// 	}
+//
+// }
--- boardproject/src/test/java/me/thewing/boardproject/BoardControllerTest.java
+++ boardproject/src/test/java/me/thewing/boardproject/BoardControllerTest.java
@@ -1,81 +1,81 @@
 package me.thewing.boardproject;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import me.thewing.boardproject.domain.BoardDto;
-import me.thewing.boardproject.service.BoardService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import me.thewing.boardproject.domain.BoardDto;
+import me.thewing.boardproject.service.BoardService;
 
 @WebMvcTest
 //@Transactional
 public class BoardControllerTest {
-    @Autowired
-    MockMvc mockMvc;
+	@Autowired
+	MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
+	@Autowired
+	ObjectMapper objectMapper;
 
-    @MockBean
-    BoardService boardService;
+	@MockBean
+	BoardService boardService;
 
-    String boardPath = "/api/board";
+	String boardPath = "/api/board";
 
-    @Test
-    void getList() throws Exception{
-        mockMvc.perform(get(boardPath+"/getlist"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                ;
-    }
+	@Test
+	void getList() throws Exception {
+		mockMvc.perform(get(boardPath + "/getlist"))
+				.andDo(print())
+				.andExpect(status().isOk())
+		;
+	}
 
-    @Test
-    void getBoard() throws Exception {
+	@Test
+	void getBoard() throws Exception {
 
-        mockMvc.perform(get(boardPath+"/getBoard/1"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                ;
-    }
+		mockMvc.perform(get(boardPath + "/getBoard/1"))
+				.andDo(print())
+				.andExpect(status().isOk())
+		;
+	}
 
-    @Test
-    void insertBoard() throws Exception{
-        System.out.println("test");
-        BoardDto test = BoardDto.builder()
-                .title("test")
-                .content("내용")
-                .writer("thewing")
-                .password(1234)
-                .regdate(LocalDateTime.now().toString())
-                .cnt(1)
-                .build();
-        mockMvc.perform(post("/api/board/insertBoard")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(test)))
-                .andDo(print())
-                .andExpect(status().isCreated())
-        ;
+	@Test
+	void insertBoard() throws Exception {
+		System.out.println("test");
+		BoardDto test = BoardDto.builder()
+				.title("test")
+				.content("내용")
+				.writer("thewing")
+				.password(1234)
+				.regdate(LocalDateTime.now().toString())
+				.cnt(1)
+				.build();
+		ResultActions resultActions = mockMvc.perform(post("/api/board/insertBoard")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(test)))
+				.andDo(print())
+				.andExpect(status().isCreated());
+		System.out.println("resultActions = " + resultActions.andReturn()
+				.getResponse()
+				.getContentAsString()
+		);
+	}
 
-    }
-
-    @Test
-    void apiTest() throws Exception {
-        // mockMvc.perform(get("https://dapi.kakao.com/v2/local/geo/coord2address.json?input_coord=WGS84&y=37.599191&x=126.716316")
-        //                     .contentType(MediaType.APPLICATION_JSON)
-    }
+	@Test
+	void apiTest() throws Exception {
+		// mockMvc.perform(get("https://dapi.kakao.com/v2/local/geo/coord2address.json?input_coord=WGS84&y=37.599191&x=126.716316")
+		//                     .contentType(MediaType.APPLICATION_JSON)
+	}
 
 }
